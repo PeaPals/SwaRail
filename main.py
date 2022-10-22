8526de50ebbba36fc06613b4653b87067584af04
Rishaab Kalra <55053464+PeaPals@users.noreply.github.com>
2022-10-22 23:09:57 +0530

    working prototype demo (version 0.1.0)

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ CommandPanel.initialize()
 
 def update():
     input_handler.check_navigations()
-    Timer.update_time()  # TODO :- uncomment this when doing simulation
+    Timer.update_time()
 
 
 SwaRailApplication.run()
